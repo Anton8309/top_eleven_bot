9811b4afe6a609230ee582fff1d97ecc9afc0dca
anton8309 <mario_83.83@mail.ru>
2024-04-10 19:16:31 +0300

    Удален закаментированный код.

--- bot/bot.py
+++ bot/bot.py
@@ -3,7 +3,7 @@ import logging
 import configs.config
 import handlers
 import inline_handlers
-import inline_keyboard
+
 from aiogram import Bot, Dispatcher
 
 
@@ -11,8 +11,7 @@ async def main():
     bot = Bot(token=configs.configs.bot_token.get_secret_value())
     dp = Dispatcher()
     dp.include_routers(handlers.router_handler, inline_handlers.router_inline_handler)
-    # dp.include_routers(handlers.router_handler, inline_handlers.router_inline_handler,
-    #                    inline_keyboard.router_inline_keyboard)
+
     await dp.start_polling(bot)
 
 
