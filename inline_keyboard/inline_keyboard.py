3566fb1111238e709ff45a92c626e2f8830f0288
anton8309 <mario_83.83@mail.ru>
2024-04-10 19:20:10 +0300

    Добавлены новые инлайн кнопки.

--- inline_keyboard/inline_keyboard.py
+++ inline_keyboard/inline_keyboard.py
@@ -1,18 +1,16 @@
 from aiogram import Router
 from aiogram.utils.keyboard import InlineKeyboardMarkup, InlineKeyboardButton
-from handlers.handler_text import HandlerText, HandlerTextGk
+from handlers.handler_text import HandlerTextGk, HandlerTextDc, HandlerTextDlDr, HandlerTextDmc, HandlerTextMc
 
 router_inline_keyboard = Router()
 
 
 class InlineKeyboardPlayers:
-    GK: str = 'gk'.upper()
-    CD: str = 'cd'.upper()
 
     @staticmethod
     def get_inline_gk():
         buttons = [
-            InlineKeyboardButton(text=HandlerTextGk.SKILLS, callback_data=InlineKeyboardPlayers.GK),
+            InlineKeyboardButton(text=HandlerTextGk.SKILLS, callback_data=HandlerTextGk.SKILLS),
             InlineKeyboardButton(text=HandlerTextGk.TRAINING, callback_data=HandlerTextGk.TRAINING),
 
         ]
@@ -22,8 +20,35 @@ class InlineKeyboardPlayers:
     @staticmethod
     def get_inline_cd():
         buttons = [
-            InlineKeyboardButton(text='навыки', callback_data=InlineKeyboardPlayers.CD),
-            InlineKeyboardButton(text='треневка', callback_data=InlineKeyboardPlayers.CD)
+            InlineKeyboardButton(text=HandlerTextDc.SKILLS, callback_data=HandlerTextDc.SKILLS),
+            InlineKeyboardButton(text=HandlerTextDc.TRAINING, callback_data=HandlerTextDc.TRAINING)
+        ]
+        inline_markup = InlineKeyboardMarkup(inline_keyboard=[buttons])
+        return inline_markup
+
+    @staticmethod
+    def get_inline_dl_dr():
+        buttons = [
+            InlineKeyboardButton(text=HandlerTextDlDr.SKILLS, callback_data=HandlerTextDlDr.SKILLS),
+            InlineKeyboardButton(text=HandlerTextDlDr.TRAINING, callback_data=HandlerTextDlDr.TRAINING)
+        ]
+        inline_markup = InlineKeyboardMarkup(inline_keyboard=[buttons])
+        return inline_markup
+
+    @staticmethod
+    def get_inline_dmc():
+        buttons = [
+            InlineKeyboardButton(text=HandlerTextDmc.SKILLS, callback_data=HandlerTextDmc.SKILLS),
+            InlineKeyboardButton(text=HandlerTextDmc.TRAINING, callback_data=HandlerTextDmc.TRAINING)
+        ]
+        inline_markup = InlineKeyboardMarkup(inline_keyboard=[buttons])
+        return inline_markup
+
+    @staticmethod
+    def get_inline_mc():
+        buttons = [
+            InlineKeyboardButton(text=HandlerTextMc.SKILLS, callback_data=HandlerTextMc.SKILLS),
+            InlineKeyboardButton(text=HandlerTextMc.TRAINING, callback_data=HandlerTextMc.TRAINING)
         ]
         inline_markup = InlineKeyboardMarkup(inline_keyboard=[buttons])
         return inline_markup
@@ -33,6 +58,39 @@ class InlineBackButton:
     @staticmethod
     def get_cmd_gk_back():
         button_back = [
-            InlineKeyboardButton(text=HandlerText.BACK, callback_data=HandlerText.BACK)]
+            InlineKeyboardButton(text=HandlerTextGk.BACK, callback_data=HandlerTextGk.BACK)
+        ]
+        back_markup = InlineKeyboardMarkup(inline_keyboard=[button_back])
+        return back_markup
+
+    @staticmethod
+    def get_cmd_dc_back():
+        button_back = [
+            InlineKeyboardButton(text=HandlerTextDc.BACK, callback_data=HandlerTextDc.BACK)
+        ]
+        back_markup = InlineKeyboardMarkup(inline_keyboard=[button_back])
+        return back_markup
+
+    @staticmethod
+    def get_cmd_dl_dr_back():
+        button_back = [
+            InlineKeyboardButton(text=HandlerTextDlDr.BACK, callback_data=HandlerTextDlDr.BACK)
+        ]
+        back_markup = InlineKeyboardMarkup(inline_keyboard=[button_back])
+        return back_markup
+
+    @staticmethod
+    def get_cmd_dmc_back():
+        button_back = [
+            InlineKeyboardButton(text=HandlerTextDmc.BACK, callback_data=HandlerTextDmc.BACK)
+        ]
+        back_markup = InlineKeyboardMarkup(inline_keyboard=[button_back])
+        return back_markup
+
+    @staticmethod
+    def get_inline_mc_back():
+        button_back = [
+            InlineKeyboardButton(text=HandlerTextMc.BACK, callback_data=HandlerTextMc.BACK)
+        ]
         back_markup = InlineKeyboardMarkup(inline_keyboard=[button_back])
         return back_markup
