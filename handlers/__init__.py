48323e0cc5e046b0064d51d7da0b3c65649a0af6
anton8309 <mario_83.83@mail.ru>
2024-04-10 19:15:20 +0300

    Импортированы кдассы с текстом для позиций игроков.

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,3 +1,8 @@
 from .handler import router_handler
 from .handler_text import HandlerText
 from .handler_text import HandlerTextGk
+from .handler_text import HandlerTextDc
+from .handler_text import HandlerTextDlDr
+from .handler_text import HandlerTextDmc
+from .handler_text import HandlerTextMc
+
