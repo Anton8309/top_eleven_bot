b6c762b5f84fba3291606944c155b0e53b51bc64
anton8309 <mario_83.83@mail.ru>
2024-04-10 19:18:27 +0300

    Добавлены классы с текстом для позиции игроков.

--- handlers/handler_text.py
+++ handlers/handler_text.py
@@ -1,8 +1,37 @@
 class HandlerText:
     HELLO: str = 'привет'.title()
-    BACK: str = '<< назад'.title()
 
 
 class HandlerTextGk:
-    SKILLS: str = 'навыки'.title()
-    TRAINING: str = 'тренеровка'.title()
+    GK: str = 'голкипер 🥅'.title()
+    SKILLS: str = 'навыки >'.title()
+    TRAINING: str = 'тренеровка >'.title()
+    BACK: str = '< назад'.title()
+
+
+class HandlerTextDc:
+    DC: str = 'центральный защитник ⚽'.title()
+    SKILLS: str = 'навыки >>'.title()
+    TRAINING: str = 'тренеровка >>'.title()
+    BACK: str = '<< назад'.title()
+
+
+class HandlerTextDlDr:
+    DLDR: str = 'правый и левый защитник ⚽'.title()
+    SKILLS: str = 'навыки >>>'.title()
+    TRAINING: str = 'тренеровка >>>'.title()
+    BACK: str = '<<< назад'.title()
+
+
+class HandlerTextDmc:
+    DMC: str = 'опорный полузащитник ⚽'.title()
+    SKILLS: str = 'навыки >>>>'.title()
+    TRAINING: str = 'тренеровка >>>>'.title()
+    BACK: str = '<<<< назад'.title()
+
+
+class HandlerTextMc:
+    MC: str = 'центральный полузащитник ⚽'.title()
+    SKILLS: str = 'навыки >>>>>'.title()
+    TRAINING: str = 'тренеровка >>>>>'.title()
+    BACK: str = '<<<<< назад'.title()
