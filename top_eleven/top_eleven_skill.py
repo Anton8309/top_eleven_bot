e4e1eb8c09d4aa23fc27d7b7456fd2d7312892e3
anton8309 <mario_83.83@mail.ru>
2024-04-10 19:23:06 +0300

    Интегрирован в код бота, исправлен код под бота, удален лишний код и родительский класс.

---
+++ top_eleven/top_eleven_skill.py
@@ -0,0 +1,85 @@
+from top_eleven.modul_skill_coach import skills_gk, skills_dl_dr, skills_dc  # Защита
+from top_eleven.modul_skill_coach import couching_gk, coaching_dl_dr, coaching_dc  # Защита
+from top_eleven.modul_skill_coach import skills_dmc, skills_mc, skills_ml_mr  # полузащита
+from top_eleven.modul_skill_coach import coaching_dmc, coaching_mc, coaching_ml_mr  # полузащита
+from top_eleven.modul_skill_coach import skills_aml_amr, skills_amc, skills_st  # атака
+from top_eleven.modul_skill_coach import coaching_aml_amr, coaching_amc, coaching_st  # атака
+
+
+class GK:
+    # GK
+    name_class = 'GK'
+    skill_gk = skills_gk
+    couch_gk = couching_gk
+
+
+GOALKEEPER = GK()
+
+
+class DLDR:
+    # DL-DR
+    name_class = 'DL-DR'
+    skill_dl_dr = skills_dl_dr
+    coach_dl_dr = coaching_dl_dr
+
+
+DEFENDER_DLDR = DLDR()
+
+
+class DC:
+    # DC
+    name_class = 'DC'
+    skill_dc = skills_dc
+    coach_dc = coaching_dc
+
+
+DEFENDER = DC()
+
+
+class DMC:
+    # DMC
+    name_class = 'DMC'
+    skill_dmc = skills_dmc
+    coach_dmc = coaching_dmc
+
+
+SUPPORTING = DMC()
+
+
+class MC:
+    # MC
+    name_class = 'MC'
+    skill_mc = skills_mc
+    coach_mc = coaching_mc
+
+
+MIDFIELDER = MC()
+
+
+class MLMR:
+    # MLMR
+    name_class = 'ML-MR'
+    skill_ml_mr = skills_ml_mr
+    coach_ml_mr = coaching_ml_mr
+
+
+class AMLAMR:
+    # AMLAMR
+
+    name_class = 'AML-AMR'
+    skill_aml_amr = skills_aml_amr
+    coach_aml_amr = coaching_aml_amr
+
+
+class AMC:
+    # AMC
+    name_class = 'AMC'
+    skill_amc = skills_amc
+    coach_amc = coaching_amc
+
+
+class ST:
+    # ST
+    name_class = 'ST'
+    skill_st = skills_st
+    coach_st = coaching_st
